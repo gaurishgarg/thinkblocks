923bfe8e2778658f8e0a50e54e16c5eb8f0533ce
gaurishgarg <gaurish_garg@outlook.com>
2026-04-21 01:48:07 +0530

    Clean commit (removed APK)

---
+++ links.js
@@ -0,0 +1,12 @@
+
+        const LINKS = {
+    apk: "https://drive.google.com/uc?export=download&id=1QCOOFlg8ooNKEC3_U9oQB_jrkBYMOZl5",
+    whatsapp_order: "https://wa.me/919463532551?text=Hi%21%20I%20want%20ThinkBlocks",
+    whatsapp_playstore: "https://wa.me/919463532551?text=Hi%21%20I%20recently%20purchased%20the%20ThinkBlocks%20Game%20and%20would%20like%20access%20to%20the%20companion%20app",
+    community: "https://chat.whatsapp.com/Kg6bd96dwKU9KPuLmAuCg9?mode=gi_t",
+    odoo_store: "https://thinkified-games.odoo.com/",
+    retail_partner: "https://wa.me/919463532551?text=Hi%21%20I%20want%20to%20buy%20ThinkBlocks%20from%20a%20store.%0ACity:%0A",
+    retail_order: "https://wa.me/919463532551?text=Hi%21%20I%20am%20interested%20in%20becoming%20a%20retail%20partner%20for%20ThinkBlocks.%0AStore%20Name:%0ACity:%0A",
+    msme: "msme.pdf",
+    playlist: "https://www.youtube.com/playlist?list=PLutC9bNfL52E_hdFEoTooKw-lHhTpib6p"
+};

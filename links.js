3a6e874df0dd4a80b4b6297e965dc2eb0c48a63c
Gaurish Garg <gaurish_garg@outlook.com>
2026-04-21 19:40:48 +0530

    Update links.js

--- links.js
+++ links.js
@@ -4,7 +4,7 @@
     whatsapp_order: "https://wa.me/919463532551?text=Hi%21%20I%20want%20ThinkBlocks",
     whatsapp_playstore: "https://wa.me/919463532551?text=Hi%21%20I%20recently%20purchased%20the%20ThinkBlocks%20Game%20and%20would%20like%20access%20to%20the%20companion%20app",
     community: "https://chat.whatsapp.com/Kg6bd96dwKU9KPuLmAuCg9?mode=gi_t",
-    odoo_store: "https://thinkified-games.odoo.com/shop/thinkblocks-2",
+    odoo_store: "https://wa.me/p/26251349564483037/919463532551",
     retail_partner: "https://wa.me/919463532551?text=Hi%21%20I%20want%20to%20buy%20ThinkBlocks%20from%20a%20store.%0ACity:%0A",
     retail_order: "https://wa.me/919463532551?text=Hi%21%20I%20am%20interested%20in%20becoming%20a%20retail%20partner%20for%20ThinkBlocks.%0AStore%20Name:%0ACity:%0A",
     msme: "msme.pdf",
